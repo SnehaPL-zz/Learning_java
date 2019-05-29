fbaa4f515c954eab65df459d6a3c69facea08c09
SnehaPL <47850031+SnehaPL@users.noreply.github.com>
2019-05-29 21:20:39 +0530

    Learning Encapsulation

--- App.java
+++ App.java
@@ -1,24 +1,45 @@
+class Plant {
+    
+    // Usually only static final members are public
+    public static final int ID = 7;
+    
+    // Instance variables should be declared private, 
+    // or at least protected.
+    private String name;
+    
+    // Only methods intended for use outside the class
+    // should be public. These methods should be documented
+    // carefully if you distribute your code.
+    public String getData() {
+        String data = "some stuff" + calculateGrowthForecast();
+        
+        return data;
+    }
+    
+    // Methods only used the the class itself should
+    // be private or protected.
+    private int calculateGrowthForecast() {
+        return 9;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
+
 
 public class App {
 
-	public static void main(String[] args) {
-		Plant plant1 = new Plant();
-		Tree tree = new Tree();
-		
-		Plant plant2 = tree;
-		
-		plant1.grow();
-		plant2.grow();
-		
-		tree.shedLeaves();
-		
-		//plant2.shedLeaves();
-		
-		doGrow(tree);
-	}
-	
-	public static void doGrow(Plant plant){
-		plant.grow();
-	}
+    public static void main(String[] args) {
+    	Plant plant = new Plant();
+    	System.out.println(plant.getData());
+    }
 
 }
